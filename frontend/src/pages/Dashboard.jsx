6bbe548d501d4a2ddf569c31667e82b55dea1a67
Ays1004 <yewaleayush@gmail.com>
2025-03-22 18:33:15 +0530

    finished frontend and connected it

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import Appbar from "../components/Appbar"
+import Balance from "../components/Balance"
+import Users from "../components/Users"
+
+const Dashboard = () => {
+    const [balance, setBalance] = useState(0);
+
+    useEffect(() => {
+        const fetchBalance = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("token")
+                    }
+                });
+                setBalance(parseInt(response.data.balance));
+            } catch (error) {
+                console.error("Error fetching balance:", error);
+            }
+        };
+
+        fetchBalance();
+    }, []);
+
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={balance} />
+            <Users />
+        </div>
+    </div>
+}
+
+export default Dashboard
